8fcfcc56812e34d044898d311139057c9718d64e
JustinPot1213 <669308066@qq.com>
2020-03-06 16:58:48 +0800

    提交第三周课堂练习
    Signed-off-by: JustinPot1213 <669308066@qq.com>

---
+++ Homework_3/Homework_3-1 3-2/Shape.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3
+{
+
+    public abstract class Shape {
+
+        static public string[] TypeOfShape = { "Square", "Rectangle", "Triangle" };
+
+        private double area;
+        public bool Legality { set; get; }
+        public double Area
+        {
+            get => area;
+            set => area = value;
+        }
+        public abstract void ShowArea();
+    }
+
+    public class Rectangle : Shape
+    {
+        private readonly int width,height;
+
+        public Rectangle(int width,int length)
+        {
+            if (width > 0 && length > 0 && length >= width)
+            {
+                this.width = width;
+                this.height = length;
+                this.Area = width * length;
+                this.Legality = true;
+            }
+            else
+            {
+                this.Legality = false;
+                //Console.WriteLine("设置参数有误。");
+            }
+        }
+
+        public override void ShowArea()
+        {
+            Console.WriteLine($"该长方形面积为{Area}");
+        }
+
+    }
+
+    public class Square : Rectangle
+    {
+        public Square(int side) : base(side, side) { }
+
+        public override void ShowArea()
+        {
+            Console.WriteLine($"该正方形面积为{Area}");
+        }
+
+    }
+
+    public class Triangle : Shape
+    {
+        private readonly int side_1,side_2,side_3;
+        public Triangle(int side_1, int side_2, int side_3)
+        {
+            if ((
+                (side_1 + side_2) > side_3) && 
+                ((side_3 + side_1) > side_2)&&
+                ((side_3 + side_2) > side_1))
+            {
+                this.side_1 = side_1;
+                this.side_2 = side_2;
+                this.side_3 = side_3;
+                double temp = (side_1 + side_2 + side_3) / 2;
+                double temp_1 = (temp * (temp - side_1) * (temp - side_2) * (temp - side_3));
+                this.Area = Math.Pow(temp_1,0.5);
+                this.Legality = true;
+            }
+            else
+            {
+                this.Legality = false;
+                //Console.WriteLine("设置参数有误。");
+            }
+        }
+
+        public override void ShowArea()
+        {
+            Console.WriteLine($"该三角面积为{Area}");
+        }
+    }
+
+}
---
+++ Homework_3/Homework_3-1 3-2/ShapeFactory.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3
+{
+    public class ShapeFactory
+    {
+        private readonly int createdNum;
+        private Shape[] products;
+        public Double Sum { set; get; }
+
+        public ShapeFactory(int createNum)
+        {
+            if (createNum > 0)
+            {
+                this.createdNum = createNum;
+                products = new Shape[createNum];
+            }
+            else Console.WriteLine("生产个数不能非正");
+        }
+
+        public void CreateShape()
+        {
+            Random rnd = new Random();
+            int randomNum;
+            string type;
+            for(int i = 0; i < createdNum; i++)
+            {
+                randomNum = rnd.Next(0, 3);
+                type=Shape.TypeOfShape[randomNum];
+                bool flag = false;
+                switch (type)
+                {
+                    case "Square":
+                        while (!flag)
+                        {
+                            this.products[i] =
+                                new Square(rnd.Next(1, 100));
+                            flag = products[i].Legality;
+                        }
+                        break;
+                    case "Rectangle":
+                        while (!flag)
+                        {
+                            this.products[i] =
+                            new Rectangle(rnd.Next(1, 100), rnd.Next(1, 100));
+                            flag = products[i].Legality;
+                        }
+                        break;
+                    case "Triangle":
+                        while (!flag)
+                        {
+                            this.products[i] =
+                            new Triangle(rnd.Next(1, 100), rnd.Next(1, 100), rnd.Next(1, 100));
+                            flag = products[i].Legality;
+                        }
+                        break;
+                }
+            }
+        }
+
+        public void GetAreaSum()
+        { 
+            for(int i = 0; i < createdNum; i++)
+            {
+                //products[i].ShowArea();
+                Sum += products[i].Area;
+            }
+        }
+    }
+
+    class Prpgram
+    {
+        public static void Main(string[] args)
+        {
+            ShapeFactory sf = new ShapeFactory(10);
+            sf.CreateShape();
+            sf.GetAreaSum();
+            Console.WriteLine($"总面积为{sf.Sum}");
+        }
+    }
+}
